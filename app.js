1fc8ffb7ce1b72d28fe67050a3ac6bd63c2f75b6
Gerar <gerarburgos1987@gmail.com>
2021-09-19 17:31:34 -0300

    gera

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve('./views/home.html'));
+});
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve('./views/login.html'));
+});
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.resolve('./views/register.html'));
+});
+
+
+app.listen(PORT, () => console.log ('servidor en puerto 3000'));
