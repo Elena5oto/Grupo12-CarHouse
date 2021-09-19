7ea7c17236bd2bc85dcc605b38645d66e84b5ac2
carlos33936079 <c.l.08.cl@gmail.com>
2021-09-19 15:22:12 -0300

    se agrega carrito de compras y logo

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ let publicPath = path.join(__dirname,"./public");
 
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT || 3000,()=>console.log("servidor en linea http://localhost:3030"));
+app.listen(process.env.PORT || 3030,()=>console.log("servidor en linea http://localhost:3030"));
 
 app.post('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
 });
 
+app.get('/carrito.html', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/carrito.html"))
+});
 
