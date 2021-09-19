4f55f793bdc3c265dcf53cad3ca1839385650e7b
Gerar <gerarburgos1987@gmail.com>
2021-09-19 20:28:05 -0300

    cambio

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static(publicPath));
 
 app.listen(process.env.PORT || 3030,()=>console.log("servidor en linea http://localhost:3030"));
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
 });
 
