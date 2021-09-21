bbaa9e46b293b3d23184165739cf3d10908a4c2f
Mitzio-roldan <mitzioroldan124@gmail.com>
2021-09-21 13:03:29 -0300

    add productdetail

--- app.js
+++ app.js
@@ -24,3 +24,7 @@ app.get('/register.html', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/register.html"))
 });
 
+app.get('/producto', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productDetail.html"))
+});
+
