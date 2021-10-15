62688c96f2f9184f14be873340e972941866f7fe
Mitzio-roldan <mitzioroldan124@gmail.com>
2021-10-14 22:05:50 -0300

    add multer and other

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers/mainController.js');
+const path = require('path');
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/images/products')
+    }, 
+    filename: function (req, file, cb) {
+        cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);
+     }
+ });
+let upload = multer({storage});
 
 router.get('/', controllers.home);
 router.get('/producto', controllers.product);
