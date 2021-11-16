a58b24fff1563ee1ba56baadb30251cb64e1444f
carlos33936079 <c.l.08.cl@gmail.com>
2021-11-15 21:16:17 -0300

    a

--- src/middlewares/recordarmeMiddleware.js
+++ src/middlewares/recordarmeMiddleware.js
@@ -5,4 +5,5 @@ function recordarmeMiddleware( req, res, next){
             req.session.userlogged
         }
 }
-module.exports = recordameMiddleware
+module.exports = recordarmeMiddleware
+                 
