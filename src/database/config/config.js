57cf38bbdee7828d95ea57160201fd53472ecbca
Mitzio-roldan <mitzioroldan124@gmail.com>
2021-12-13 12:39:06 -0300

    others

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": 'mitzio19',
+    "password": '',
     "database": "eduardo_roldan_database",
     "host": "127.0.0.1",
     "dialect": "mysql"
